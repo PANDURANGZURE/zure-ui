f8f70bde12930bf9b5e0ae888604a0eef92d947e
PANDURANGZURE <pandurangzure6@gmail.com>
2026-03-18 15:55:08 +0530

    as

--- app/_meta.js
+++ app/_meta.js
@@ -1,5 +1,14 @@
 export default {
-  Documentation:"Documentation",
-  Components : "Components",
-  
+  Documentation: "Documentation",
+
+  Components: "Components",
+
+  '---': {
+    type: 'separator'
+  },
+
+  license: {
+    title: 'LICENSE',
+    href: 'https://github.com/PANDURANGZURE/zure-ui/blob/master/LICENSE'
+  }
 }
